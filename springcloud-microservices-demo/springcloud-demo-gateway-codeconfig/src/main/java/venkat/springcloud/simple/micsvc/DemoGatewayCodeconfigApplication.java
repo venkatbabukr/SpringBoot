da16a296200e88dd42d530b2c3d79730eb125723
Venkatesh Babu KR <>
2023-07-04 16:53:42 +0530

    Added many metrics and health management properties

--- springcloud-microservices-demo/springcloud-demo-gateway-codeconfig/src/main/java/venkat/springcloud/simple/micsvc/DemoGatewayCodeconfigApplication.java
+++ springcloud-microservices-demo/springcloud-demo-gateway-codeconfig/src/main/java/venkat/springcloud/simple/micsvc/DemoGatewayCodeconfigApplication.java
@@ -2,6 +2,8 @@ package venkat.springcloud.simple.micsvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.StripPrefixGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,6 +12,14 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 @EnableEurekaClient
 public class DemoGatewayCodeconfigApplication {
+
+	// TODO: Need to see why this is not working...
+//	@Bean
+//	public GatewayFilter stripPrefixFilter() {
+//		StripPrefixGatewayFilterFactory.Config prefixFilterConfig = new StripPrefixGatewayFilterFactory.Config();
+//		prefixFilterConfig.setParts(1);
+//		return new StripPrefixGatewayFilterFactory().apply(prefixFilterConfig);
+//	}
 
     @Bean
     public RouteLocator appRoutes(RouteLocatorBuilder rlb) {
